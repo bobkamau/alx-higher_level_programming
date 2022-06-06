54b2c024ebb4874b3ea16d9c6e90a86f9cc869a9
bobkamau <robertmwangi369@gmail.com>
2022-06-06 11:04:23 -0700

    task 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+n_list(my_list, idx, element):
+        length = len(my_list)
+
+            new_list = my_list[:]
+
+                if 0 <= idx < length:
+                            new_list[idx] = element
+
+                                return (new_list)
