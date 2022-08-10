d6081cf1b98a8964299ee5b4e396b607beea6393
bobkamau <Bob@bobbymwangi.com>
2022-08-11 00:10:44 +0300

    18

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -9,11 +9,11 @@ SELECT DISTINCT `title`
        ON g.`id` = s.`genre_id`
        WHERE t.`title` NOT IN
              (SELECT `title`
-		                FROM `tv_shows` AS t
-					             INNER JOIN `tv_show_genres` AS s
-								     ON s.`show_id` = t.`id`
+                FROM `tv_shows` AS t
+	             INNER JOIN `tv_show_genres` AS s
+		     ON s.`show_id` = t.`id`
 
-										     INNER JOIN `tv_genres` AS g
-												     ON g.`id` = s.`genre_id`
-														     WHERE g.`name` = "Comedy")
-													 ORDER BY `title`;
+		     INNER JOIN `tv_genres` AS g
+		     ON g.`id` = s.`genre_id`
+		     WHERE g.`name` = "Comedy")
+ ORDER BY `title`;
