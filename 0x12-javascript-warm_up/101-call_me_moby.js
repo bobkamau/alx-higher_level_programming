bb1e2855db33cda3d55b27f9e467988523af5b7f
bobkamau <Bob@bobbymwangi.com>
2022-09-05 21:15:10 +0300

    15

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // JS Script
-//
-// exports.callMeMoby = function (x, theFunction) {
-//   for (let i = 0; i < x; i++) {
-//       theFunction();
-//         }
-//         };
+
+exports.callMeMoby = function (x, theFunction) {
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
+};
