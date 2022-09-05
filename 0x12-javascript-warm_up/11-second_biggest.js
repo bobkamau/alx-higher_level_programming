60843c4df74279402b7b972d2b62c1f189946381
bobkamau <Bob@bobbymwangi.com>
2022-09-05 21:27:04 +0300

    11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,24 +1,8 @@
-const args = process.argv.slice(2);
-const list = [];
-let i = 0;
-let j = 0;
-let max = 0;
-let second = 0;
-if (args.length === 0 || args.length === 1) {
-  console.log(0);
+#!/usr/bin/node
+if (process.argv.length <= 3) {
+  console.log('0');
 } else {
-  for (i = 0; i < args.length; i++) {
-    list[i] = parseInt(args[i]);
-  }
-  for (j = 0; j < list.length; j++) {
-    if (list[j] > max) {
-      max = list[j];
-    }
-  }
-  for (j = 0; j < list.length; j++) {
-    if (list[j] > second && list[j] < max) {
-      second = list[j];
-    }
-  }
+  const arr = process.argv.slice(2).map(Number);
+  const second = arr.sort(function (a, b) { return b - a; })[1];
   console.log(second);
 }
