316bea8759e8317c374db96c50bc8c28bec37733
bobkamau <Bob@bobbymwangi.com>
2022-09-05 21:01:01 +0300

    10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 function Factorial (number) {
-	  if (!number) {
-		      return (1);
-		    }
-	  if (number !== 0) {
-		      return (number * Factorial(number - 1));
-		    }
+  if (!number) {
+    return (1);
+  }
+  if (number !== 0) {
+    return (number * Factorial(number - 1));
+  }
 }
 const { argv } = require('process');
 const number = parseInt(argv[2]);
