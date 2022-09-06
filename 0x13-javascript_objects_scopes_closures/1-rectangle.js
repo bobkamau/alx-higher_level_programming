e3e838f7de17b4ce08ad75d3b481cabe66a441da
bobkamau <Bob@bobbymwangi.com>
2022-09-06 20:23:13 +0300

    1

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 // Class Rectangle that defines rectangle
-//
-// module.exports = class Rectangle {
-//   constructor (width, height) {
-//       this.width = width;
-//           this.height = height;
-//             }
-//             };
+
+module.exports = class Rectangle {
+  constructor (width, height) {
+    this.width = width;
+    this.height = height;
+  }
+};
