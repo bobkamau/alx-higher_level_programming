86acfca63fe330893a38ec702a4ff0674ebc508e
bobkamau <Bob@bobbymwangi.com>
2022-09-06 20:35:06 +0300

    4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,34 +1,34 @@
 #!/usr/bin/node
 
 const Rectangle = class {
-	  constructor (w, h) {
-		      if (w <= 0 || h <= 0) {
-			            w = undefined;
-			            h = undefined;
-			          }
-		      if (w !== undefined && h !== undefined) {
-			            this.width = w;
-			            this.height = h;
-			          }
-		    }
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+      w = undefined;
+      h = undefined;
+    }
+    if (w !== undefined && h !== undefined) {
+      this.width = w;
+      this.height = h;
+    }
+  }
 
-	  print () {
-		      const printer = 'X'.repeat(this.width);
-		      for (let i = 0; i < this.height; i++) {
-			            console.log(printer);
-			          }
-		    }
+  print () {
+    const printer = 'X'.repeat(this.width);
+    for (let i = 0; i < this.height; i++) {
+      console.log(printer);
+    }
+  }
 
-	  rotate () {
-		      const tmp = this.width;
-		      this.width = this.height;
-		      this.height = tmp;
-		    }
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
 
-	  double () {
-		      this.width *= 2;
-		      this.height *= 2;
-		    }
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
 };
 
 module.exports = Rectangle;
