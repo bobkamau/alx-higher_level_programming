594f5124d358b58cf4c1a018e91923b4de3a7cc9
bobkamau <Bob@bobbymwangi.com>
2022-09-06 21:02:48 +0300

    12

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,7 +3,7 @@
 const { dict } = require('./101-data');
 
 const myValue = Object.entries(dict).reduce((acc, [key, value]) => {
-	  acc[value] = acc[value] ? [...acc[value], key] : [key];
-	  return acc;
+  acc[value] = acc[value] ? [...acc[value], key] : [key];
+  return acc;
 }, {});
 console.log(myValue);
