4a0e75770f6c46c4eed4544bcd73cb9e84b94c86
bobkamau <Bob@bobbymwangi.com>
2022-09-06 20:43:43 +0300

    6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,16 +2,16 @@
 
 const Square5 = require('./5-square');
 class Square extends Square5 {
-	  charPrint (c) {
-		      if (c === undefined) {
-			            super.print();
-			          } else {
-					        const printer = c.repeat(this.width);
-					        for (let i = 0; i < this.height; i++) {
-							        console.log(printer);
-							      }
-					      }
-		    }
+  charPrint (c) {
+    if (c === undefined) {
+      super.print();
+    } else {
+      const printer = c.repeat(this.width);
+      for (let i = 0; i < this.height; i++) {
+        console.log(printer);
+      }
+    }
+  }
 }
 module.exports = Square5;
 module.exports = Square;
