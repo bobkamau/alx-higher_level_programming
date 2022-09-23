ea6e8518e05f00ad2a96c1192ae8aeb5fe8ecc3e
bobkamau <Bob@bobbymwangi.com>
2022-09-23 21:23:11 +0300

    5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
-
 '''task 5 script'''
 
-
-
 if __name__ == '__main__':
+    import requests
+    import sys
 
-        import requests
-
-            import sys
-
-
-
-                res = requests.get(sys.argv[1])
-
-                    print(res.headers.get('X-Request-Id'))
+    res = requests.get(sys.argv[1])
+    print(res.headers.get('X-Request-Id'))
