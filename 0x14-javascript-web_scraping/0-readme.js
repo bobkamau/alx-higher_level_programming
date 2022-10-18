1a9caa496a78c66cac569b055cc9637284bcf49e
bobkamau <Bob@bobbymwangi.com>
2022-10-18 21:38:34 +0300

    0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,19 +1,10 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-
 const file = process.argv[2];
-
 fs.readFile(file, 'utf-8', function (err, data) {
-
-	  if (err) {
-
-		      console.log(err);
-
-		    } else {
-
-			        console.log(data);
-
-			      }
-
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 });
