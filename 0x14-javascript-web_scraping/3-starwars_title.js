7622378db97c279690f06fd9dc08d6360310f6db
bobkamau <Bob@bobbymwangi.com>
2022-10-18 21:48:30 +0300

    3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,29 +1,15 @@
 #!/usr/bin/node
-
 const request = require('request');
-
-
 
 const episodeId = process.argv[2];
 
-
-
 if (parseInt(episodeId) < 8) {
+  const url = 'https://swapi-api.hbtn.io/api/films/' + episodeId;
 
-	  const url = 'https://swapi-api.hbtn.io/api/films/' + episodeId;
-
-
-
-	  request(url, (err, res, body) => {
-
-		      if (err) {
-
-			            return console.log(err);
-
-			          }
-
-		      console.log(JSON.parse(body).title);
-
-		    });
-
+  request(url, (err, res, body) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(JSON.parse(body).title);
+  });
 }
